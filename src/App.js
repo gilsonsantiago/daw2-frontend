7a327deeb7e7a70baaf3ac78630ca92006e2d9e1
Gilson Santiago <gs.thiago@hotmail.com>
2024-08-23 19:30:07 -0300

    Alteração do cadastro
    Atualização das novas funcionalidades.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import './App.css';
 import PainelNavegacao from './Components/PainelNavegacao';
 
 function App() {
+
   return (
+
     <div className="App">
+
        <PainelNavegacao />
+
        <Rotas />
+
     </div>
+
   );
+  
 }
 
 export default App;
