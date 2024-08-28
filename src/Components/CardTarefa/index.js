2d93f7fb552b359446870bea15961309fabdadfc
Gilson Santiago <gs.thiago@hotmail.com>
2024-08-27 21:15:47 -0300

    Formatação da tabela

--- src/Components/CardTarefa/index.js
+++ src/Components/CardTarefa/index.js
@@ -5,23 +5,35 @@ import Grid from '@mui/material/Grid';
 import { Box } from '@mui/material';
 
 const columns = [
-  { field: 'id', headerName: '#', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
+    { 
+      field: 'id', 
+      headerName: '#', 
+      width: 70
+    },
+    { 
+      field: 'firstName', 
+      headerName: 'First name', 
+      width: 130 
+    },
+    { 
+      field: 'lastName', 
+      headerName: 'Last name', 
+      width: 130 
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    },
 ];
 
 const rows = [
@@ -49,7 +61,7 @@ function CardTarefa() {
       }}
     >
       <Grid
-        sx={{ width: '75%'}}
+        sx={{ width: '75%' }}
         alignItems="center"
         container
         justifyContent="center"
