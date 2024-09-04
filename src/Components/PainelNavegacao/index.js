571d6e7fa6c0001a702fb80b41f545da6b1e7817
Gilson Santiago <gs.thiago@hotmail.com>
2024-09-03 21:38:26 -0300

    Refatorando Login

--- src/Components/PainelNavegacao/index.js
+++ src/Components/PainelNavegacao/index.js
@@ -10,8 +10,11 @@ import MenuIcon from '@mui/icons-material/Menu';
 function PainelNavegacao() {
   return (
     <Box sx={{ flexGrow: 1 }}>
+      
       <AppBar position="static">
+        
         <Toolbar>
+
           <IconButton
             size="large"
             edge="start"
@@ -19,14 +22,20 @@ function PainelNavegacao() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+
+          <MenuIcon />
+
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
+
           <Button color="inherit">Login</Button>
+
         </Toolbar>
+
       </AppBar>
+
     </Box>
   );
 }
--- src/pages/PaginaLogin/index.js
+++ src/pages/PaginaLogin/index.js
@@ -14,6 +14,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props) {
+
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
@@ -30,7 +31,8 @@ function Copyright(props) {
 
 const defaultTheme = createTheme();
 
-export default function SignIn() {
+function PaginaLogin() {
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -42,8 +44,11 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+
       <Container component="main" maxWidth="xs">
+
         <CssBaseline />
+
         <Box
           sx={{
             marginTop: 8,
@@ -51,13 +56,14 @@ export default function SignIn() {
             flexDirection: 'column',
             alignItems: 'center',
           }}
-        >
+         >
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
+
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
@@ -91,22 +97,33 @@ export default function SignIn() {
             >
               Sign In
             </Button>
+
             <Grid container>
+
               <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
+              
               <Grid item>
                 <Link href="#" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
+
             </Grid>
+
           </Box>
+
         </Box>
+
         <Copyright sx={{ mt: 8, mb: 4 }} />
+
       </Container>
+
     </ThemeProvider>
   );
 }
+
+export default PaginaLogin;
